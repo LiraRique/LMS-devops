fdb05a228b66906aa82410db317ef8b5bd414bc0
Henrique Lira <lira.apren@gmail.com>
2018-11-06 20:09:18 -0200

    2

--- TecWeb_LMS/settings.py
+++ TecWeb_LMS/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '4sd%u*ai-b8k54n4al9@+qn$qy#5ecdon-9mlwyf(#v%#1hw82'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -42,7 +42,6 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
