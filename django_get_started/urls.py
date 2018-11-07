7ab082ece30bc238551b054bc2e931528d83433b
Henrique Lira da Silva <1801224@SALAS.AULAS>
2018-11-07 20:57:20 -0200

    3

--- django_get_started/urls.py
+++ django_get_started/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from django.urls import path
 from app.views import *
+from datetime import datetime
+from django.conf.urls import patterns, url
+from app.forms import BootstrapAuthenticationForm
 
 urlpatterns = [
     #PAGINAS DAS HOMES
