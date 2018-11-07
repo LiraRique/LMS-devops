5447ca5441538a1ed0d54e618e6520b670955c9a
Henrique Lira da Silva <1801224@SALAS.AULAS>
2018-11-07 21:16:50 -0200

    e

--- django_get_started/urls.py
+++ django_get_started/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path
 from app.views import *
-
+'''
 urlpatterns = [
     #PAGINAS DAS HOMES
     path('', index),
@@ -39,3 +39,49 @@ urlpatterns = [
 
     path('admin/', admin.site.urls),
 ]
+'''
+
+"""
+Definition of urls for django_get_started.
+"""
+
+from datetime import datetime
+from django.conf.urls import patterns, url
+from app.forms import BootstrapAuthenticationForm
+
+# Uncomment the next lines to enable the admin:
+# from django.conf.urls import include
+# from django.contrib import admin
+# admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    url(r'^$', 'app.views.home', name='home'),
+    url(r'^contato$', 'app.views.contato', name='contato'),
+    url(r'^about', 'app.views.about', name='about'),
+    url(r'^cadastro_cursos', 'app.views.cadastro_cursos', name='cadastro_cursos'),
+    url(r'^login/$',
+        'django.contrib.auth.views.login',
+        {
+            'template_name': 'app/login.html',
+            'authentication_form': BootstrapAuthenticationForm,
+            'extra_context':
+            {
+                'title':'Log in',
+                'year':datetime.now().year,
+            }
+        },
+        name='login'),
+    url(r'^logout$',
+        'django.contrib.auth.views.logout',
+        {
+            'next_page': '/',
+        },
+        name='logout'),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    # url(r'^admin/', include(admin.site.urls)),
+)
