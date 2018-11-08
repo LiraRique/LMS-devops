b775050ef7fea64bc39d817466dd93c361457fe1
Henrique Lira da Silva <1801224@SALAS.AULAS>
2018-11-08 20:51:24 -0200

    v1.1.1

--- app/views.py
+++ app/views.py
@@ -90,5 +90,5 @@ def cadastrar_prof(request):
 def cadastrar_aluno(request):
     return render(request,'DashBoard/area_coordenador/cadastrar_aluno.html')
 
-def conta_coor(request):
-    return render(request,'DashBoard/area_coordenador/conta_coor.html')
+def conta_coordenador(request):
+    return render(request,'DashBoard/area_coordenador/conta_coordenador.html')
